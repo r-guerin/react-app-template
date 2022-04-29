dca61fefbf103a65452defee1989a914178ad80f
rguerin.pro@gmail.com <rguerin.pro@gmail.com>
2022-04-29 14:49:17 +0200

    :hammer: refactor(lint): use default eslint parser for js files

--- scripts/webpack/analyze.js
+++ scripts/webpack/analyze.js
@@ -1,6 +1,6 @@
-const dev = require('./dev');
 const { merge } = require('webpack-merge');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const dev = require('./dev');
 
 module.exports = merge(dev, {
   plugins: [new BundleAnalyzerPlugin()],
--- scripts/webpack/prod.js
+++ scripts/webpack/prod.js
@@ -1,6 +1,5 @@
-const common = require('./common');
-const path = require('path');
 const { merge } = require('webpack-merge');
+const common = require('./common');
 
 module.exports = merge(common, {
   mode: 'production',
